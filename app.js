72ce578736ce8d2b41fd5048a14aede2573af43b
z4hed <zhedthegreat@gmail.com>
2022-01-29 23:43:43 -0500

    News Stuff Works

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
 
-//                 ------------------SORTING--------------------
+//                 ------------------SORTING & INPUTTING--------------------
 //stock data
 const stock = {
     name:"AAPL",
@@ -22,6 +22,8 @@ o_Stock_List.push(stock);
 //                  ------------------Internal Mechanics---------------
 const btn_left = document.querySelector('#left_button');
 const btn_right = document.querySelector('#right_button');
+var chart_link = document.getElementById('Chart_Search');
+
 
 function switch_right()
 {   
@@ -32,13 +34,13 @@ function switch_right()
     const card_title = document.querySelector('#stock_title');
     const card_temp = document.querySelector('#stock_description_1');
     event.preventDefault();
-    card_title.innerText = s_name;
-    card_temp.innerText = s_market_temp;
-
+    card_title.innerHTML = s_name;
+    card_temp.innerHTML = s_market_temp;
+    
+    
     //Charts Info
+    chart_link.setAttribute('href',"https://www.google.com/search?q="+o_Stock_List[0].name+"+"+"Stocks")
 
-
-    //New info
     
 }
 
@@ -52,6 +54,35 @@ function switch_left()
     card_title.innerText = s_name;
 }
 
+//News info
+const newsList= document.querySelector('.news-list')
+
+function retreive(e){
+
+    const apiKey = '0a73adc839c64b13b9e7aa791f23a769'
+    let topic = o_Stock_List[0].name + " Stocks"
+    let url = "https://newsapi.org/v2/everything?q="+topic+"&apiKey="+apiKey
+
+    fetch(url).then((res)=>{
+        return res.json()
+    }).then((data)=>{
+        console.log(data);
+        data.articles.forEach(article => {
+            let li = document.createElement('li')
+            let a = document.createElement('a')
+            a.setAttribute('href', article.url)
+            a.setAttribute('target', '_blank')
+            a.textContent = article.title;
+
+            li.appendChild(a)
+            newsList.appendChild(li)
+        });
+    })
+    
+}
+
+
 
 btn_left.addEventListener('click',switch_left)
 btn_right.addEventListener('click',switch_right)
+btn_right.addEventListener('click',retreive)
